11a98831053e4cc17740f9c0c270589de70ea115
Sid Kedha <116390123+sid618@users.noreply.github.com>
2023-07-30 20:28:39 -0400

    first check in

---
+++ conditional.py
@@ -0,0 +1,13 @@
+#if 3 < 2:
+#    print("hello")
+#else:
+#   print("the condition isnt true")
+
+
+age = 16 
+if age <= 15:
+    print("you are younger than 16")
+elif age == 16:
+    print('you are 16')
+else:
+    print('you are older than 16')
---
+++ forloops.py
@@ -0,0 +1,9 @@
+#list1 = ['apples', 'bananas', 'cherries']
+#tup1 = (2,6,10)
+
+#for item in tup1:
+#    print(item)
+
+for i in range(0,5):
+    for j in range(0,3):
+        print(i * j)
